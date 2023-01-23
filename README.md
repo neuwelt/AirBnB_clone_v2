bcf719a21dde99a95d697a962d46c3236809b0c9
neuwelt <ineuwelt@neuwelt.co>
2023-01-23 15:41:08 +0000

    fixed bugs from old projects

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the 3rd version of my AirBnB cloning
+And emphasis is on making it better
+Authored by Cantong jr
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Starts a Flask web app listening on 0.0.0.0:5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    """Displays “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays “HBNB”"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Displays “C ” followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/(<text>)', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Displays “Python ”, followed by the value of the text variable"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
